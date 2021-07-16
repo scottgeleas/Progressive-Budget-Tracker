c8827b4e3c28dec7cd8b659f53f4d2c31d6d4a9e
Scott Geleas <scottgeleas97@gmail.com>
2021-07-16 13:13:38 -0400

    formating

--- routes/api.js
+++ routes/api.js
@@ -1,35 +1,35 @@
 const router = require("express").Router();
 const Transaction = require("../models/transaction.js");
 
-router.post("/api/transaction", ({body}, res) => {
-  Transaction.create(body)
-    .then(dbTransaction => {
-      res.json(dbTransaction);
-    })
-    .catch(err => {
-      res.status(500).json(err);
-    });
+router.post("/api/transaction", ({ body }, res) => {
+    Transaction.create(body)
+        .then(dbTransaction => {
+            res.json(dbTransaction);
+        })
+        .catch(err => {
+            res.status(500).json(err);
+        });
 });
 
-router.post("/api/transaction/bulk", ({body}, res) => {
+router.post("/api/transaction/bulk", ({ body }, res) => {
     console.log(body)
-  Transaction.insertMany(body)
-    .then(dbTransaction => {
-      res.json(dbTransaction);
-    })
-    .catch(err => {
-      res.status(500).json(err);
-    });
+    Transaction.insertMany(body)
+        .then(dbTransaction => {
+            res.json(dbTransaction);
+        })
+        .catch(err => {
+            res.status(500).json(err);
+        });
 });
 
 router.get("/api/transaction", (req, res) => {
-  Transaction.find({}).sort({date: -1})
-    .then(dbTransaction => {
-      res.json(dbTransaction);
-    })
-    .catch(err => {
-      res.status(500).json(err);
-    });
+    Transaction.find({}).sort({ date: -1 })
+        .then(dbTransaction => {
+            res.json(dbTransaction);
+        })
+        .catch(err => {
+            res.status(500).json(err);
+        });
 });
 
 module.exports = router;
